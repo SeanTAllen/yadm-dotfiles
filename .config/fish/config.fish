42ce4459608bb2ba7fe084b1672b908b03455722
Sean T. Allen <sean@monkeysnatchbanana.com>
2019-01-19 15:44:49 -0500

    Update to my usual powerline-go prompt
    (except it has some new "lock" icon at the beginning that I'm not sure
    what it is.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ set -gx EDITOR 'vim'
 
 ## use powerline-go for command prompt
 function fish_prompt
-    ~/bin/powerline-go -error $status -shell bare
+    ~/bin/powerline-go -error $status -shell bare -modules venv,ssh,cwd,perms,git,jobs -cwd-max-depth 1 -truncate-segment-width 8
 end
 
 ## turn off fish greeting
