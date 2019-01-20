b9511e8cdb6155383391cba1632af17e8dcf18dd
Sean T. Allen <sean@monkeysnatchbanana.com>
2019-01-20 08:12:42 -0500

    Add fish port of z command
    Allows you to jump to recent directories. Set up `fisher`, a fish
    package manager in the process. Fisher will be installed automatically
    if not installed and will then install any packages it needs that are
    listed in the fishfile.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,12 @@
+# install `fisher` package manager
+# https://github.com/jorgebucaran/fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+# editor
 set -gx EDITOR 'vim'
 
 ## aliases
