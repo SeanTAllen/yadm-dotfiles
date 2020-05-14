accfa42cf358560c31097edde820a1a509e5d105
Sean T. Allen <sean@seantallen.com>
2020-05-13 23:01:21 -0400

    Switch to starship for prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -46,10 +46,9 @@ alias mkidr     mkdir
 
 set -gx PATH $HOME/bin $PATH
 
-## use powerline-go for command prompt
-function fish_prompt
-    ~/bin/powerline-go -error $status -shell bare -modules host,ssh,venv,cwd,perms,git,jobs -cwd-max-depth 1 -truncate-segment-width 8
-end
-
 ## turn off fish greeting
 set fish_greeting
+
+## turn on starship for prompt
+starship init fish | source
+
