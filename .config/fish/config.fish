e476bcc089099e35801d8327ba38ef0525d542f6
Sean T. Allen <sean@seantallen.com>
2021-03-09 22:09:55 -0500

    Git commit signing

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,10 @@ end
 # editor
 set -gx EDITOR 'vim'
 
+# allow GPG to prompt for passphrase
+
+set -x GPG_TTY (tty)
+
 ## aliases
 
 # git
@@ -48,6 +52,7 @@ alias ls       'ls -v --color'
 ## path
 
 set -gx PATH $HOME/.local/share/ponyup/bin $HOME/bin $PATH
+
 ## turn off fish greeting
 set fish_greeting
 
